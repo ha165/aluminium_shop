9d3f36dcfc6f017b7976e413b13b9799d5733c12
ha165 <lumumbaharmony@gmail.com>
2026-05-11 17:48:08 +0300

    create db_seeds

--- priv/repo/seeds/users.exs
+++ priv/repo/seeds/users.exs
@@ -1,6 +1,7 @@
 # priv/repo/seeds/users.exs
 
 alias AluminiumShop.Repo
+alias AluminiumShop.Accounts
 alias AluminiumShop.Accounts.{User, Role}
 
 admin_role =
@@ -10,29 +11,28 @@ staff_role =
   Repo.get_by!(Role, name: "employee")
 
 # Admin
-Accounts.create_user(
+Accounts.create_user!(
   %User{}
   |> User.changeset(%{
     first_name: "System",
     last_name: "Admin",
     phone: "0700000000",
     email: "admin@shop.com",
-    hashed_password: Bcrypt.hash_pwd_salt("password123"),
+    password: "password123",
     role_id: admin_role.id
-  }),
-  on_conflict: :nothing
+  })
 )
 
 # 500 staff users
 Enum.each(1..500, fn _ ->
-  Accounts.create_user(
+  Accounts.create_user!(
     %User{}
     |> User.changeset(%{
       first_name: Faker.Person.first_name(),
       last_name: Faker.Person.last_name(),
       phone: Faker.Phone.EnUs.phone(),
       email: Faker.Internet.email(),
-      hashed_password: Bcrypt.hash_pwd_salt("password123"),
+      password: "password123",
       role_id: staff_role.id
     })
   )
