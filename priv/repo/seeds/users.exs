95dd422dc6a9f5abbd29f029502ae411ae9ac033
ha165 <lumumbaharmony@gmail.com>
2026-05-14 10:43:08 +0300

    update customer_migration

--- priv/repo/seeds/users.exs
+++ priv/repo/seeds/users.exs
@@ -11,9 +11,8 @@ staff_role =
   Repo.get_by!(Role, name: "employee")
 
 # Admin
-Accounts.create_user!(
-  %User{}
-  |> User.changeset(%{
+{:ok, _admin} =
+  Accounts.create_user(%{
     first_name: "System",
     last_name: "Admin",
     phone: "0700000000",
@@ -21,19 +20,18 @@ Accounts.create_user!(
     password: "password123",
     role_id: admin_role.id
   })
-)
 
 # 500 staff users
 Enum.each(1..500, fn _ ->
-  Accounts.create_user!(
-    %User{}
-    |> User.changeset(%{
-      first_name: Faker.Person.first_name(),
-      last_name: Faker.Person.last_name(),
-      phone: Faker.Phone.EnUs.phone(),
-      email: Faker.Internet.email(),
-      password: "password123",
-      role_id: staff_role.id
-    })
-  )
+  case Accounts.create_user(%{
+         first_name: Faker.Person.first_name(),
+         last_name: Faker.Person.last_name(),
+         phone: Faker.Phone.EnUs.phone(),
+         email: Faker.Internet.email(),
+         password: "password123",
+         role_id: staff_role.id
+       }) do
+    {:ok, _user} -> :ok
+    {:error, changeset} -> IO.inspect(changeset.errors, label: "Error creating user")
+  end
 end)
