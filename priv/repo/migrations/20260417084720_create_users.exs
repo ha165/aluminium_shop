a2229aa295319af21d3505db4cea8f43e8a31608
ha165 <lumumbaharmony@gmail.com>
2026-05-14 08:06:52 +0300

    update users_migration

--- priv/repo/migrations/20260417084720_create_users.exs
+++ priv/repo/migrations/20260417084720_create_users.exs
@@ -6,12 +6,16 @@ defmodule AluminiumShop.Repo.Migrations.CreateUsers do
       add :id, :binary_id, primary_key: true
       add :email, :string
       add :hashed_password, :string
-      add :role_id, references(:roles, on_delete: :nothing)
+      add :first_name, :string
+      add :last_name, :string
+      add :phone, :string
+      add :role_id, references(:roles, type: :binary_id, on_delete: :nothing)
 
       timestamps(type: :utc_datetime)
     end
 
     create unique_index(:users, [:email])
+    create unique_index(:users, [:phone])  # You have this in your schema
     create index(:users, [:role_id])
   end
 end
