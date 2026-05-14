0f547cc0da69f3c9952964e27729539b6e58dc72
ha165 <lumumbaharmony@gmail.com>
2026-05-14 09:11:51 +0300

    update customer_migration

--- priv/repo/migrations/20260504110428_create_customers.exs
+++ priv/repo/migrations/20260504110428_create_customers.exs
@@ -2,12 +2,15 @@ defmodule AluminiumShop.Repo.Migrations.CreateCustomers do
   use Ecto.Migration
 
   def change do
-    create table(:customers) do
+    create table(:customers, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :name, :string
       add :phone, :string
       add :email, :string
 
       timestamps(type: :utc_datetime)
     end
+    create unique_index(:customers, [:email])
+    create unique_index(:customers, [:phone])
   end
 end
